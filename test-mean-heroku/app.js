87ddac6e5bcf6f39808cddbbf594efef04938cda
Sanket Bhangale <66669902+sanketbhangale43@users.noreply.github.com>
2021-09-12 12:41:19 +0530

    initial commit

---
+++ test-mean-heroku/app.js
@@ -0,0 +1,30 @@
+var cors = require('cors')
+const cookieparser = require("cookie-parser");
+const dotEnv = require("dotenv");
+const express = require('express')
+
+const path = require('path');
+const app = express();
+app.use(cors())
+
+
+app.use(express.json())
+app.use(cookieparser());
+
+
+/* -------------------------------- API URLs -------------------------------- */
+
+
+
+
+
+
+/* ------------------------------- Production ------------------------------- */
+const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname + '/dist')));
+app.get('**', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${POST}`);
+})
